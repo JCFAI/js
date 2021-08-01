532bb8d8715d0d4a4bbc8d15915c11b8f0900836
JCF <jorgecarlosfossati@gmail.com>
2021-08-01 03:02:01 -0300

    counter in RS

--- script.rs
+++ script.rs
@@ -1,1 +1,16 @@
-alert("Hi");
+      let counter = 0;
+      function count() {
+        counter++;
+        const heading = document.querySelector("h1");
+        heading.innerHTML = counter;
+
+        if (counter % 10 == 0) {
+          document.querySelector(
+            "#tenths"
+          ).innerHTML = `La cuenta llego a ${counter}`;
+        }
+      }
+      document.addEventListener("DOMContentLoaded", function () {
+        //document.querySelector("button").onclick = count;
+        document.querySelector("button").addEventListener("click", count);
+      });
